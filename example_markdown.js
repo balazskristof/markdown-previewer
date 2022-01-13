cde6196c60766e822e0f67b1533e407db48e9886
bmey9z <bmey9z@inf.elte.hu>
2022-01-13 07:07:38 +0100

    Separated example Markdown into its own file

---
+++ example_markdown.js
@@ -0,0 +1,22 @@
+const example = `# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+This is a paragraph.
+*This text is italicized.*
+**This is bold text.**
+***This text combines both.***
+~~This text is struckthrough.~~
+
+> This is a quote.
+
+Use the command \`git clone https://github.com/balazskristof/markdown-previewer\` to clone this repo.
+
+This project was created with:
+ - [Ace](https://ace.c9.io/)
+ - [Marked](https://marked.js.org/)
+ - [github-markdown-css](https://github.com/sindresorhus/github-markdown-css)
+ - [Bootstrap Icons](https://icons.getbootstrap.com/)`;
