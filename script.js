dc1373302066d0d6238b860fb5ced843bc97fb4e
bmey9z <bmey9z@inf.elte.hu>
2022-01-13 06:22:25 +0100

    Parsing works

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// Marked
+marked.setOptions({gfm: true});
+
 // Ace
 let editor = ace.edit('editor');
+editor.on('change', e => {
+    document.querySelector('#preview').innerHTML = marked.parse(editor.getValue());
+});
 
 // Buttons
 let copy = document.querySelector('#copy');
 let github = document.querySelector('#github');
-
 copy.addEventListener('click', () => {
-    console.log('copy to clipboard');
+    navigator.clipboard.writeText(marked.parse(editor.getValue()));
 });
-
 github.addEventListener('click', () => {
-    console.log('open github repo');
+    window.open('https://github.com/balazskristof/markdown-previewer', '_blank');
 });
